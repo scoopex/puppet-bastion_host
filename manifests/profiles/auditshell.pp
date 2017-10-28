016aa38b7f5a05e0b6a3b74b9572dee36e945cdc
Marc Schoechlin <marc.schoechlin@globalways.net>
2017-10-28 21:02:18 +0200

    Added tool to replay sessions

--- manifests/profiles/auditshell.pp
+++ manifests/profiles/auditshell.pp
@@ -1,9 +1,9 @@
 # == Class: bastion_host::profiles::auditshell
 #
 class bastion_host::profiles::auditshell(
-	$file_download_base = 'https://raw.githubusercontent.com/scoopex/scriptreplay_ng/audit-shell/helpers',
+	$file_download_base = 'https://raw.githubusercontent.com/scoopex/scriptreplay_ng/audit-shell/',
 ){
-    ensure_packages([ 'bsdutils', 'apparmor-utils', 'dialog' ])
+    ensure_packages([ 'bsdutils', 'apparmor-utils', 'dialog' , 'perl-doc' , 'libterm-readkey-perl' ])
 
     file { '/var/log/auditshell/':
       ensure  => 'directory',
@@ -25,8 +25,18 @@ class bastion_host::profiles::auditshell(
 			match   => '/usr/local/bin/auditshell',
 		}
 
+		exec{'/usr/local/bin/scriptreplay':
+		 	command => "/usr/bin/wget -q ${file_download_base}/scriptreplay -O /usr/local/bin/scriptreplay",
+		 	creates => '/usr/local/bin/scriptreplay',
+		}->
+		file{'/usr/local/bin/scriptreplay':
+      owner => 'root',
+      group => 'root',
+		 	mode  => '0755',
+		}
+
 		exec{'/usr/local/bin/auditshell':
-		 	command => '/usr/bin/wget -q ${file_download_base}/auditshell -O /usr/local/bin/auditshell',
+		 	command => "/usr/bin/wget -q ${file_download_base}/helpers/auditshell -O /usr/local/bin/auditshell",
 		 	creates => '/usr/local/bin/auditshell',
 		}->
 		file{'/usr/local/bin/auditshell':
@@ -36,7 +46,7 @@ class bastion_host::profiles::auditshell(
 		}
 
 		exec{'/etc/apparmor.d/usr.local.bin.auditshell':
-		 	command => '/usr/bin/wget -q ${file_download_base}/usr.local.bin.auditshell -O /etc/apparmor.d/usr.local.bin.auditshell',
+		 	command => "/usr/bin/wget -q ${file_download_base}/helpers/usr.local.bin.auditshell -O /etc/apparmor.d/usr.local.bin.auditshell",
 		 	creates => '/etc/apparmor.d/usr.local.bin.auditshell',
 		}->
 		file{'/etc/apparmor.d/usr.local.bin.auditshell':
